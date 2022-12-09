b71ac71c295594a1498624aa2999e4cbeec7ee03
avhodgso <118221571+avhodgso@users.noreply.github.com>
2022-12-09 14:11:44 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,360 @@
-let mySprite = sprites.create(img`
-    . . . . . . . e e e e . . . . . 
-    . . . . . e e 4 5 5 5 e e . . . 
-    . . . . e 4 5 6 2 2 7 6 6 e . . 
-    . . . e 5 6 6 7 2 2 6 4 4 4 e . 
-    . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
-    . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
-    . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
-    e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
-    e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
-    e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
-    e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
-    e 5 e c 5 4 5 4 5 5 5 e e . . . 
-    e 5 e e 5 5 5 5 5 4 e . . . . . 
-    4 5 4 e 5 5 5 5 e e . . . . . . 
-    . 4 5 4 5 5 4 e . . . . . . . . 
-    . . 4 4 e e e . . . . . . . . . 
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f e e e e e e e f f . 
+        f f e f e e e e e e f f 
+        f f f e e e e e e e e f 
+        f f f e e e e e e f e f 
+        f f f f e e e e f f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        . f f f f f f f f f f . 
+        . e f f f f f f f f e . 
+        e 4 f b b b b b b f 4 e 
+        4 d f d d d d d d c d 4 
+        4 4 f 6 6 6 6 6 6 f 4 4 
+        . . . f f f f f f . . . 
+        . . . f f . . f f . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f e e e e e e e f f . 
+        f e e f e e e e e e f f 
+        f f f e e e e e e e e f 
+        f f e e e e e e e f e f 
+        f f f e e e e f f f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        . f f f f f f f f f f . 
+        . e f f f f f f f f e . 
+        . 4 f b b b b b f e 4 e 
+        . 4 f d d d d d e d d 4 
+        . e f f f f f f e e 4 . 
+        . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f e e e e e e e f f . 
+        f f e f e e e e e e f f 
+        f f f e e e e e e e e f 
+        f f f f e e e e e f e f 
+        f f f f f e e e e f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        . f f f f f f f f f f . 
+        . e f f f f f f f f e . 
+        e 4 e f b b b b b f 4 . 
+        4 d d e d d d d d f 4 . 
+        . 4 e e f f f f f f e . 
+        . . . . . . . f f f . . 
+        `],
+    100,
+    true
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f f e e e e e e f f . 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d d e f . 
+        . f f e 4 4 4 4 e f f . 
+        e 4 f b 1 1 1 1 b f 4 e 
+        4 d f 1 1 1 1 1 1 f d 4 
+        4 4 f 6 6 6 6 6 6 f 4 4 
+        . . . f f f f f f . . . 
+        . . . f f . . f f . . . 
+        `],
+    100,
+    true
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . f f f f f . . . . 
+        . . f e e e e e f f . . 
+        . f e e e e e e e f f . 
+        f e e e e e e e f f f f 
+        f e e 4 e e e f f f f f 
+        f e e 4 4 e e e f f f f 
+        f f e 4 4 4 4 4 f f f f 
+        f f e 4 4 f f 4 e 4 f f 
+        . f f d d d d 4 d 4 f . 
+        . . f b b d d 4 f f f . 
+        . . f e 4 4 4 e e f . . 
+        . . f 1 1 1 e d d 4 . . 
+        . . f 1 1 1 e d d e . . 
+        . . f 6 6 6 f e e f . . 
+        . . . f f f f f f . . . 
+        . . . . . f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . . f e e e e e f f f . 
+        . f e e e e e e e f f f 
+        f e e e e e e e f f f f 
+        f e e 4 e e e f f f f f 
+        f e e 4 4 e e e f f f f 
+        f f e 4 4 4 4 4 f f f f 
+        . f e 4 4 f f 4 e 4 f f 
+        . . f d d d d 4 d 4 f . 
+        . . f b b d e e f f f . 
+        . . f e 4 e d d 4 f . . 
+        . . f 1 1 e d d e f . . 
+        . f f 6 6 f e e f f f . 
+        . f f f f f f f f f f . 
+        . . f f f . . . f f . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . . f e e e e e f f f . 
+        . f e e e e e e e f f f 
+        f e e e e e e e f f f f 
+        f e e 4 e e e f f f f f 
+        f e e 4 4 e e e f f f f 
+        f f e 4 4 4 4 4 f f f f 
+        . f e 4 4 f f 4 e 4 f f 
+        . . f d d d d 4 d 4 f f 
+        . . f b b d d 4 f f f . 
+        . . f e 4 4 4 e d d 4 . 
+        . . f 1 1 1 1 e d d e . 
+        . f f 6 6 6 6 f e e f . 
+        . f f f f f f f f f f . 
+        . . f f f . . . f f . . 
+        `],
+    100,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . f f f f f . . . 
+        . . f f e e e e e f . . 
+        . f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f f 
+        . f 4 d 4 d d d d f f . 
+        . f f f 4 d d b b f . . 
+        . . f e e 4 4 4 e f . . 
+        . . 4 d d e 1 1 1 f . . 
+        . . e d d e 1 1 1 f . . 
+        . . f e e f 6 6 6 f . . 
+        . . . f f f f f f . . . 
+        . . . . f f f . . . . . 
+        `],
+    100,
+    true
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . f f f f f . . . . 
+        . . f e e e e e f f . . 
+        . f e e e e e e e f f . 
+        f e e e e e e e f f f f 
+        f e e 4 e e e f f f f f 
+        f e e 4 4 e e e f f f f 
+        f f e 4 4 4 4 4 f f f f 
+        f f e 4 4 f f 4 e 4 f f 
+        . f f d d d d 4 d 4 f . 
+        . . f b b d d 4 f f f . 
+        . . f e 4 4 4 e e f . . 
+        . . f 1 1 1 e d d 4 . . 
+        . . f 1 1 1 e d d e . . 
+        . . f 6 6 6 f e e f . . 
+        . . . f f f f f f . . . 
+        . . . . . f f f . . . . 
+        `],
+    100,
+    true
+    )
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileDarkGrass3, function (sprite, location) {
+    game.over(true)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e e f . . 
+        f f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f . 
+        f f 4 d 4 d d d d f . . 
+        . f f f 4 d d b b f . . 
+        . 4 d d e 4 4 4 e f . . 
+        . e d d e 1 1 1 1 f . . 
+        . f e e f 6 6 6 6 f f . 
+        . f f f f f f f f f f . 
+        . . f f . . . f f f . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e e f . . 
+        f f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f . 
+        . f 4 d 4 d d d d f . . 
+        . f f f e e d b b f . . 
+        . . f 4 d d e 4 e f . . 
+        . . f e d d e 1 1 f . . 
+        . f f f e e f 6 6 f f . 
+        . f f f f f f f f f f . 
+        . . f f . . . f f f . . 
+        `,img`
+        . . . . f f f f f . . . 
+        . . f f e e e e e f . . 
+        . f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f f 
+        . f 4 d 4 d d d d f f . 
+        . f f f 4 d d b b f . . 
+        . . f e e 4 4 4 e f . . 
+        . . 4 d d e 1 1 1 f . . 
+        . . e d d e 1 1 1 f . . 
+        . . f e e f 6 6 6 f . . 
+        . . . f f f f f f . . . 
+        . . . . f f f . . . . . 
+        `],
+    100,
+    true
+    )
+})
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f e e e e e e e f f . 
+        f f e f e e e e e e f f 
+        f f f e e e e e e e e f 
+        f f f e e e e e e f e f 
+        f f f f e e e e f f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        . f f f f f f f f f f . 
+        . e f f f f f f f f e . 
+        e 4 f b b b b b b f 4 e 
+        4 d f d d d d d d c d 4 
+        4 4 f 6 6 6 6 6 6 f 4 4 
+        . . . f f f f f f . . . 
+        . . . f f . . f f . . . 
+        `],
+    100,
+    true
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f f e e e e e e f f . 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d d e f . 
+        . f f e 4 4 4 4 e f f . 
+        e 4 f b 1 1 1 1 b f 4 e 
+        4 d f 1 1 1 1 1 1 f d 4 
+        4 4 f 6 6 6 6 6 6 f 4 4 
+        . . . f f f f f f . . . 
+        . . . f f . . f f . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e f f f . 
+        f f f e e e e e e f f f 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d 4 e f e 
+        f f f e 4 4 4 4 d d 4 e 
+        e 4 f b 1 1 1 e d d e . 
+        . . f 6 6 6 6 f e e . . 
+        . . f f f f f f f . . . 
+        . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e f f f . 
+        f f f e e e e e e f f f 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        e f e 4 d b b d d e f . 
+        e 4 d d 4 4 4 4 e f f f 
+        . e d d e 1 1 1 b f 4 e 
+        . . e e f 6 6 6 6 f . . 
+        . . . f f f f f f f . . 
+        . . . . . . . f f f . . 
+        `],
+    100,
+    true
+    )
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
     `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`level1`)
